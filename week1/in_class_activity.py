22f5656a7860e4b7b0b226e62007c57fb26c1a85
Lucas Fadersen <V092953@tafe.wa.edu.au>
2023-07-19 12:11:08 +0800

    end class

---
+++ week1/in_class_activity.py
@@ -0,0 +1,8 @@
+crazy_list = [1, 2, [3, 4], [5, [100, 200, ['hello']], 23, 11], 1, 7]
+print(crazy_list[3][1][2][0])
+
+animals = ['Lion', 'Tiger', 'Elephant', 'Giraffe', 'Cheetah']
+
+# turnary expression
+# list comperhention
+# dicconary comperhentiom
