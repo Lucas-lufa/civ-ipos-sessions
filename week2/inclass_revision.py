d09f54f077f0df526ab41393abda1b1337eb6ff8
Lucas Fadersen <V092953@tafe.wa.edu.au>
2023-07-26 12:34:12 +0800

    save

--- week2/inclass_revision.py
+++ week2/inclass_revision.py
@@ -9,7 +9,7 @@ In-class revision
  How can you import only a specific function or class from a module in Python? What is the syntax for this?
     use the from a python file, then import function name
  How would you explain Python's parameter-passing mechanism? Is it more similar to pass-by-value or pass-by-reference?
-    If immutable more similar to pass-by-value if mutable pass-by-reference
+    pass-by-reference
  Given the following Python code, what will be the output and why?
  def modify_list(lst):
     lst.append("new")
