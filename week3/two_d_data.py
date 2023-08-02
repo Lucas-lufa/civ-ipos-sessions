5941fce9027470d1d4c87c6dcc3de6aff0d9d193
Lucas <lucasfad@hotmail.com>
2023-08-02 12:37:53 +0800

    inclass

---
+++ week3/two_d_data.py
@@ -0,0 +1,15 @@
+def lucas(row:int, col:int, values=None):
+    """ """
+    two_d = [] * row
+    for _ in two_d:
+        two_d[_} = [] * col
+
+def make_2d(row:int, col:int, value=None):
+    two_d = []
+    for _ in range(row):
+        elem = []
+        for _ in range(col):
+            elem.append(value)
+
+
+
